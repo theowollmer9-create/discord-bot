a6bda02443074c896149845de9d78502de460c35
theowollmer9-create <theowollmer9@gmail.com>
2026-05-05 18:39:08 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+ROLE_AGENT = "⚙️ Agent"
+
+ROLE_FUEHRUNG = [
+    "👑 Minister für Staatssicherheit",
+    "🏛️ Stellv. Minister",
+    "🛡️ Hauptabteilungsleiter",
+    "🏅 Abteilungsleiter"
+]
+
+CATEGORY_TICKETS = "🔴 |  TICKETS"
+CATEGORY_SUPPORT = "🆘 SUPPORT"
+
+SUPPORT_CHANNELS = [
+"📨・SUPPORT 1",
+"📨・SUPPORT 2",
+"📨・SUPPORT 3",
+"📨・SUPPORT 4",
+"📨・SUPPORT 5",
+"📨・SUPPORT 6",
+"📨・SUPPORT 7",
+"📨・SUPPORT 8",
+"📨・SUPPORT 9",
+"📨・SUPPORT 10"
+]
+
+KENNZEICHEN_REGEX = r"[A-Z]{4}-\d{2}"
